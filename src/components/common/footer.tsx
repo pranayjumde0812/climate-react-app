927315c5692f69775f00bf4ba6e9f6ad833e9629
Pranay Jumde <pranay.jumde@qodequay.com>
2025-04-15 17:16:09 +0530

    created footer

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t background-blur py-12 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto px-4 text-center text-gray-500">
+        <p>Made By Pranay for Learning</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
