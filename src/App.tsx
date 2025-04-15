e29c98fb9fa5d6e799dfee7b0b990a59e4c1e4de
Pranay Jumde <pranay.jumde@qodequay.com>
2025-04-15 16:14:30 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/common/layout";
 import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Layout>Hello</Layout>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
       </ThemeProvider>
     </BrowserRouter>
   );
