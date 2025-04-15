3adaa75a42383ce51e39ffcd179877a3b205a8c7
Pranay Jumde <pranay.jumde@qodequay.com>
2025-04-15 14:39:54 +0530

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div>Start of application</div>;
+  return (
+    <div>
+      Start of application
+      <Button>Subscribe</Button>
+    </div>
+  );
 }
 
 export default App;
